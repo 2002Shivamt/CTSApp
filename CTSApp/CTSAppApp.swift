9590726ca8650c74792c29e729718cc4f366837a
2002Shivamt <shivam.tewari@searce.com>
2026-02-12 14:24:38 +0530

    Initial Commit

---
+++ CTSApp/CTSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTSAppApp.swift
+//  CTSApp
+//
+//  Created by Shivam Tewari on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CTSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
